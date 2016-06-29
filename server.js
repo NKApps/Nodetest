20add7f93bff11abdbe1f8f3bca5bcd357e2aa7d
Luis Enrique De Casas Garcia <luis.casas@SofttekClients.com>
2016-06-29 12:59:49 -0500

    Fresh start

--- server.js
+++ server.js
@@ -1,63 +1,50 @@
-'use strict';
-
-/*
-var cl = console.log;
-console.log = function(){
-  console.trace();
-  cl.apply(console,arguments);
-};
-*/
-
-process.env.NODE_CONFIG_DIR = './config/env';
-
-// Requires meanio .
-var mean = require('meanio');
-var cluster = require('cluster');
-var deferred = require('q').defer();
+var http = require("http");
+var fs = require("fs");
+var path = require("path");
+var mime = require("mime");
 
 
-// Code to run if we're in the master process or if we are not in debug mode/ running tests
-
-if ((cluster.isMaster) &&
-  (process.execArgv.indexOf('--debug') < 0) &&
-  (process.env.NODE_ENV!=='test') && (process.env.NODE_ENV!=='development') &&
-  (process.execArgv.indexOf('--singleProcess')<0)) {
-//if (cluster.isMaster) {
-
-    console.log('for real!');
-    // Count the machine's CPUs
-    var cpuCount = process.env.CPU_COUNT || require('os').cpus().length;
-
-    // Create a worker for each CPU
-    for (var i = 0; i < cpuCount; i += 1) {
-        console.log ('forking ',i);
-        cluster.fork();
-    }
-
-    // Listen for dying workers
-    cluster.on('exit', function (worker) {
-        // Replace the dead worker, we're not sentimental
-        console.log('Worker ' + worker.id + ' died :(');
-        cluster.fork();
-
-    });
-
-// Code to run if we're in a worker process
-} else {
-
-    var workerId = 0;
-    if (!cluster.isMaster)
-    {
-        workerId = cluster.worker.id;
-    }
-// Creates and serves mean application
-    mean.serve({ workerid: workerId /* more options placeholder*/ }, function (app) {
-      var config = app.getConfig();
-      var port = config.https && config.https.port ? config.https.port : config.http.port;
-      console.log('Mean app started on port ' + port + ' (' + process.env.NODE_ENV + ') cluster.worker.id:', workerId);
-
-      deferred.resolve(app);
-    });
+function send404(response) {
+  response.writeHead(404, {"Content-type" : "text/plain"});
+  response.write("Error 404: resource not found");
+  response.end();
 }
 
-module.exports = deferred.promise;
+
+function sendPage(response, filePath, fileContents) {
+  response.writeHead(200, {"Content-type" : mime.lookup(path.basename(filePath))});
+  response.end(fileContents);
+}
+
+function serverWorking(response, absPath) {
+  fs.exists(absPath, function(exists) {
+    if (exists) {
+      fs.readFile(absPath, function(err, data) {
+        if (err) {
+          send404(response)
+        } else {
+          sendPage(response, absPath, data);
+        }
+      });
+    } else {
+      send404(response);
+    }
+  });
+}
+
+
+var server = http.createServer(function(request, response) {
+  var filePath = false;
+
+  if (request.url == '/') {
+    filePath = "public/index.html";
+  } else {
+    filePath = "public" + request.url;
+  }
+
+  var absPath = "./" + filePath;
+  serverWorking(response, absPath);
+});
+
+var port_number = server.listen(process.env.PORT || 3000);
+
