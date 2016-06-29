dc61ba43070cd7100f431b750344229fa702b19e
Luis Enrique De Casas Garcia <luis.casas@SofttekClients.com>
2016-06-29 13:27:49 -0500

    freshhhh

--- server.js
+++ server.js
@@ -1,54 +1,23 @@
-var http = require("http");
-var fs = require("fs");
-var path = require("path");
-var mime = require("mime");
+var express = require('express');
+var app = express();
 
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
 
+// set the view engine to ejs
+app.set('view engine', 'ejs');
 
+// make express look in the public directory for assets (css/js/img)
+app.use(express.static(__dirname + '/public'));
 
-function sendPage(response, filePath, fileContents) {
-  response.writeHead(200, {"Content-type" : mime.lookup(path.basename(filePath))});
-  response.end(fileContents);
-}
+// set the home page route
+app.get('/', function(req, res) {
 
-
-
-
-var server = http.createServer(function(request, response) {
-  var filePath = false;
-
-  if (request.url == '/') {
-    filePath = "public/index.html";
-  } else {
-    filePath = "public" + request.url;
-  }
-
-  var absPath = "./" + filePath;
-  serverWorking(response, absPath);
+	// ejs render automatically looks in the views folder
+	res.render('index');
 });
 
-var port_number = server.listen(process.env.PORT || 3000);
-
-
-
-
-
-
-
-var port_number = server.listen(process.env.PORT || 3000);
-
-function serverWorking(response, absPath) {
-  fs.exists(absPath, function(exists) {
-    if (exists) {
-      fs.readFile(absPath, function(err, data) {
-        if (err) {
-          send404(response)
-        } else {
-          sendPage(response, absPath, data);
-        }
-      });
-    } else {
-      send404(response);
-    }
-  });
-}
+app.listen(port, function() {
+	console.log('Our app is running on http://localhost:' + port);
+});
