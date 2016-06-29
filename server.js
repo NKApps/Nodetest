c76fbf3eaf000c5d722cab5830dc112360935a83
Luis Enrique De Casas Garcia <luis.casas@SofttekClients.com>
2016-06-29 13:09:44 -0500

    aasd

--- server.js
+++ server.js
@@ -16,35 +16,3 @@ function sendPage(response, filePath, fileContents) {
   response.end(fileContents);
 }
 
-function serverWorking(response, absPath) {
-  fs.exists(absPath, function(exists) {
-    if (exists) {
-      fs.readFile(absPath, function(err, data) {
-        if (err) {
-          send404(response)
-        } else {
-          sendPage(response, absPath, data);
-        }
-      });
-    } else {
-      send404(response);
-    }
-  });
-}
-
-
-var server = http.createServer(function(request, response) {
-  var filePath = false;
-
-  if (request.url == '/') {
-    filePath = "public/index.html";
-  } else {
-    filePath = "public" + request.url;
-  }
-
-  var absPath = "./" + filePath;
-  serverWorking(response, absPath);
-});
-
-var port_number = server.listen(process.env.PORT || 3000);
-
