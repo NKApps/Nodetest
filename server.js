9a56aa570adf9cbef8ec392e36b2ffe11b52b402
Luis Enrique De Casas Garcia <luis.casas@SofttekClients.com>
2016-06-29 13:15:27 -0500

    rolback

--- server.js
+++ server.js
@@ -16,3 +16,44 @@ function sendPage(response, filePath, fileContents) {
   response.end(fileContents);
 }
 
+
+
+
+var server = http.createServer(function(request, response) {
+  var filePath = false;
+
+  if (request.url == '/') {
+    filePath = "public/index.html";
+  } else {
+    filePath = "public" + request.url;
+  }
+
+  var absPath = "./" + filePath;
+  serverWorking(response, absPath);
+});
+
+var port_number = server.listen(process.env.PORT || 3000);
+
+
+
+
+
+
+
+var port_number = server.listen(process.env.PORT || 3000);
+
+function serverWorking(response, absPath) {
+  fs.exists(absPath, function(exists) {
+    if (exists) {
+      fs.readFile(absPath, function(err, data) {
+        if (err) {
+          send404(response)
+        } else {
+          sendPage(response, absPath, data);
+        }
+      });
+    } else {
+      send404(response);
+    }
+  });
+}
